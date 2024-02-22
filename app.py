04e434ecd5eeabc635246ccb2b9a5a29397efae2
khadarbashakarumanchi <khadarbashakarumanchi@gmail.com>
2024-02-22 18:17:53 +0530

    message

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask, redirect, url_for , render_template, request , session
-from datetime import timedelta
+from datetime import timedelta 
+from flask import flash
 
 
 app = Flask(__name__)
-app.secret_key = "goodmorning"
+app.secret_key = "nerchuko"
 app.permanent_session_lifetime = timedelta(minutes=5)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html")   
 
 @app.route("/login" , methods = ["POST" , "GET"])
 def login():
@@ -16,9 +17,11 @@ def login():
         session.permanent = True
         user = request.form['usr_name']
         session['user'] = user 
+        flash("Login Successful !!") 
         return redirect(url_for("user" )) 
     else:
         if "user" in session:
+            flash("You are already logged in !!") 
             return redirect(url_for("user"))
         return render_template("login.html")
 
@@ -27,13 +30,15 @@ def login():
 def user():
     if 'user' in session:
         user = session['user'] 
-        return f"You logged in as {user}!"
-    else:
+        return render_template("user.html" , user = user) 
+    else: 
+        flash("you are not logged in!")
         return redirect(url_for("login"))
 
 @app.route("/logout")
 def logout():
     session.pop("user" , None)
+    flash("You have been logged out!" , 'info') 
     return redirect(url_for("login"))
  
 if __name__ == '__main__':
