46620b674ba65bab7bd36efbe243e8b83b8402f0
khadarbashakarumanchi <khadarbashakarumanchi@gmail.com>
2024-03-21 15:45:47 +0530

    message

---
+++ weather.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+from pprint import pprint
+import requests
+import os
+
+load_dotenv()
+
+def get_current_weather(city="Hyderabad"):  # Default value added for city parameter
+    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'  # Changed units to metric
+    weather_data = requests.get(request_url).json()
+    return weather_data
+
+if __name__ == "__main__":
+    print("\n***Get Current Weather Conditions ***\n")
+    city = input("\n Please Enter a city name: ")
+    
+    weather_data = get_current_weather(city)
+    print("\n")
+    pprint(weather_data)
